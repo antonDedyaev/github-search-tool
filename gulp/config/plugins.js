72b5d07512ecf5d90ed7abb55309fe35d4fdfcc6
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-03-15 20:13:57 +0300

    set up gulp and install deps

---
+++ gulp/config/plugins.js
@@ -0,0 +1,11 @@
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import browserSync from 'browser-sync';
+
+const plugins = {
+  plumber,
+  notify,
+  browserSync,
+};
+
+export default plugins;
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/extensions */
+import gulp from 'gulp';
+import path from './gulp/config/path.js';
+import plugins from './gulp/config/plugins.js';
+
+import copy from './gulp/tasks/copy.js';
+import reset from './gulp/tasks/reset.js';
+import html from './gulp/tasks/html.js';
+import server from './gulp/tasks/server.js';
+import scss from './gulp/tasks/scss.js';
+import js from './gulp/tasks/js.js';
+
+global.app = {
+  path,
+  gulp,
+  plugins,
+};
+
+function watcher() {
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+}
+
+const mainTasks = gulp.parallel(copy, html, scss, js);
+
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+
+gulp.task('default', dev);
